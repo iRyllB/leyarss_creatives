31e58f20b79aef6f0e3c09cb04b9c1f8cfe9971a
iRyllB <iryllb123@gmail.com>
2026-03-09 10:48:17 +0800

    updated header and arranged alignment

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,22 @@
-import "../styles/Header.css"
+import "../styles/Header.css";
 
 export default function Header() {
   return (
-    <header className="header">
+    <header className="site-header">
+      <div className="site-header-container">
+        <a href="#home" className="site-logo" aria-label="Go to homepage">
+          <img src="/logo2.png" alt="Leyarss Creatives Logo" />
+        </a>
 
-      <div className="header-container">
-
-        <div className="logo">
-          <img src="/logo.png" />
-        </div>
-
-        <nav className="nav">
-          <a>HOME</a>
-          <a>ABOUT</a>
-          <a>SERVICES</a>
-          <a>PORTFOLIO</a>
+        <nav className="site-nav">
+          <a href="#home">HOME</a>
+          <a href="#about">ABOUT</a>
+          <a href="#services">SERVICES</a>
+          <a href="#portfolio">PORTFOLIO</a>
         </nav>
 
-        <button className="contact-btn">
-          Contact Us
-        </button>
-
+        <button className="site-contact-btn">Contact Us</button>
       </div>
-
     </header>
-  )
+  );
 }
