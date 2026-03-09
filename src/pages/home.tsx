ffe9049488a59ed0eec09ba2e8a6088242b0c618
iRyllB <iryllb123@gmail.com>
2026-03-09 11:15:21 +0800

    added navbar opacity

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "../styles/Home.css";
 
 export default function Home() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('.site-header');
+      if (window.scrollY > 10) {
+        header.classList.add('site-header--scrolled');
+      } else {
+        header.classList.remove('site-header--scrolled');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <>
       <Header />
